72600dd0745d122f6a2c1ab731d8c80d46abf7bf
piyush121 <piyushchaudhary07@gmail.com>
2016-01-13 17:54:45 -0500

    In pursuit

---
+++ src/Ch_1/one_5.java
@@ -0,0 +1,32 @@
+package Ch_1;
+
+public class one_5 {
+	
+	public static void main(String[] args) {
+		String str="abcdefgggggggg";
+		StringBuffer strb=new StringBuffer();
+		int count=1;
+		int i=0;
+		for(i=0;i<str.length()-1;i++)
+			if(str.charAt(i)==str.charAt(i+1))
+				{
+					
+					count++;
+				}
+			else
+				{
+					
+				    strb.append(str.charAt(i));
+				    strb.append(count);
+					count=1;
+				}
+		strb.append(str.charAt(i));
+	    strb.append(count);
+	    if(strb.length()>str.length())
+	    	System.out.println(str);
+	    else
+	    	System.out.println(strb);
+	}
+	
+		
+}
