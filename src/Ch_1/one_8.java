c36c7f07b064a5dfe815a5f0bed132108cf44d47
piyush121 <piyushchaudhary07@gmail.com>
2016-01-14 00:15:15 -0500

    In pursuit

---
+++ src/Ch_1/one_8.java
@@ -0,0 +1,17 @@
+package Ch_1;
+
+public class one_8 {
+	
+	public static void main(String[] args) {
+		String str= "waterbottle";
+		String str1="erbo4ttlewat";
+		
+		String str3=str+str;
+		if(str3.indexOf(str1)>=0)
+			System.out.println(true);
+		else
+			System.out.println(false);
+
+	}
+
+}
