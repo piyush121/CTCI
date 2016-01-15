e9fa3279a806aed0e12cfe37167826c9ec79e4b4
piyush121 <piyushchaudhary07@gmail.com>
2016-01-14 20:36:40 -0500

    Another one

--- src/Ch_2/Two_2.java
+++ src/Ch_2/Two_2.java
@@ -1,5 +1,31 @@
 package Ch_2;
 
 public class Two_2 {
-
+	static int value=0;
+	
+	public static int klast(LLNode head,int k) //Recursive method. Kind of difficult to grasp in one go.
+	{
+		int i=0;
+		LLNode ptr=head;
+		if(head==null)
+			return 0;
+		if(ptr!=null)
+			 i=klast(ptr.next,k)+1;
+		if(i==k)
+			System.out.println(k + " to last node is :" + ptr.key);
+				
+		return i;
+	}
+	
+	public static void main(String[] args) {
+		LLNode head=new LLNode(1);
+		head.addNode(head, 2);
+		head.addNode(head, 3);
+		head.addNode(head, 4);
+		head.addNode(head, 5);
+		head.print(head);
+		System.out.println("\n");
+		klast(head,4);
+		
+	}
 }
