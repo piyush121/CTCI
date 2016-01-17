7c5996b47497adb8fad9f5901ad038ba63c2c6ac
piyush121 <piyushchaudhary07@gmail.com>
2016-01-16 19:28:09 -0500

    Ch-3 going on

---
+++ src/Ch_3/three_5.java
@@ -0,0 +1,28 @@
+package Ch_3;
+
+import java.util.Stack;
+// Didn't imlement peek as it was quite obvious.
+public class three_5 {
+Stack<Integer> stack1=new Stack<>();
+Stack<Integer> stack2=new Stack<>();
+
+
+public Integer enqueue(Integer data)
+{
+	stack1.add(data);
+	return data;
+}
+
+public Integer dequeue(Integer data)
+{
+	if(stack1.size()==1)
+		stack1.pop();
+	else
+		stack2.addAll(stack1);
+	stack1.clear();
+	Integer value=stack2.pop();
+	stack1.addAll(stack2);
+	stack2.clear();
+	return value;
+}
+}
