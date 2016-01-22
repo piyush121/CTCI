bc7044be89b2f1be9d33bfba940c3a80f57dc785
piyush121 <piyushchaudhary07@gmail.com>
2016-01-21 22:06:56 -0500

    ch-4 almost done

--- src/ch_4/four_8.java
+++ src/ch_4/four_8.java
@@ -4,7 +4,7 @@
 package ch_4;
 
 /**
- * @author piyus
+ * @author piyush
  *
  */
 public class four_8 {
@@ -12,6 +12,27 @@ public class four_8 {
 	/**
 	 * @param args
 	 */
+	public boolean match(treeNode node, treeNode root)
+	{
+		if(node==null && root==null)	// nothing left to compare
+			return true;
+		if(root==null || node==null)	//if one of them is empty.
+			return false;
+		if(node!=root)
+			return false;
+		
+		return match(node.right,root.right)&& match(node.left,root.left);
+	}
+	public boolean subTreeCheck(treeNode t1,treeNode root)
+	{
+		if(t1==null || root==null)
+			return false;
+		if(t1==root)
+			if( match(t1,root))
+				return true;
+		return subTreeCheck(t1, root.left)|| subTreeCheck(t1, root.right);
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
