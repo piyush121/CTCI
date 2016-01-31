3c679c1be944ae07fb36f2e3798463ee79979818
piyush121 <piyushchaudhary07@gmail.com>
2016-01-30 20:51:09 -0500

    More Practice

--- src/ch_11/eleven_Three.java
+++ src/ch_11/eleven_Three.java
@@ -3,7 +3,10 @@ package ch_11;
 public class eleven_Three {
 	
 	public static int find(int[] arr,int x)
-	{
+	{	if(arr.length==1 && arr[0]==x)
+			return 0;
+		if(arr.length==1 && arr[0]!=x)
+			return -1;
 		int low=0;
 		int high=arr.length-1;
 		int mid;
@@ -13,16 +16,16 @@ public class eleven_Three {
 			if(arr[mid]==x)
 			return mid;
 			
-			if(arr[mid]>arr[low])
+			if(arr[mid]>arr[low])		//left normally ordered.
 			{
-				if(x>arr[low] && x<arr[mid])
+				if(x>=arr[low] && x<=arr[mid])
 					high=mid-1;
 				else
 					low=mid+1;
 			}
-			else if(arr[mid]<arr[high])
+			else if(arr[mid]<arr[high])	//right normally ordered.
 			{
-				if(x>arr[mid] && x<arr[high])
+				if(x>=arr[mid] && x<=arr[high])
 					low=mid+1;
 				else
 					high=mid-1;
@@ -31,27 +34,35 @@ public class eleven_Three {
 			{
 				if(arr[mid]!=arr[high])
 					low=mid+1;
+				else
+				{	
+					int[] left=new int[mid-low+1];
+					int[] right=new int[high-mid];
+					int p=0; int q=0;
+					for(int i=low;i<=mid;i++)
+						{
+							left[p]=arr[i];
+							p++;
+						}
+					for(int j=mid+1;j<=high;j++)
+						{
+						right[q]=arr[j];
+						q++;
+						}
+					int result =find(left,x);
+					if( result==-1)
+						return find(right,x);
+					else 
+						return result;
+					}
 			}
-			else
-			{	
-				int[] left=new int[mid-low+1];
-				int[] right=new int[high-mid+1];
-				for(int i=low;i<=mid;i++)
-					left[i]=arr[i];
-				for(int j=mid+1;j<=high;j++)
-					right[j]=arr[j];
-				int result =find(left,x);
-				if( result==-1)
-					return find(right,x);
-				else 
-					return result;
-				}
+			
 				
 		}
 		return -1;
 	}
 	public static void main(String[] args) {
-		int[] A={15,16,19,20,25,1,2,3,4,5,6,10,14};
-		System.out.println(find(A,10));
+		int[] A={3,3,3,4,5,3};
+		System.out.println(find(A,4));
 	}
 }
