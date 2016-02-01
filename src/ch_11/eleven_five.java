59205e8881bfa9f0ea3cb1f3dd1dbfef50858d10
piyush121 <piyushchaudhary07@gmail.com>
2016-01-31 23:45:28 -0500

    Practice

--- src/ch_11/eleven_five.java
+++ src/ch_11/eleven_five.java
@@ -5,22 +5,22 @@ public class eleven_five {
 	public static int search(String[] sarr,String target,int low, int mid,int high)
 	{
 		int mid1=(high+low)/2;
+		if(low>high)
+			return -1;
 		if(sarr[mid].equals(target))
 			return mid;
 		if(sarr[mid]=="" && mid<sarr.length-1)
 			return search(sarr,target,low,mid+1,high);
 		if(sarr[mid]=="" && mid>0)
 			return search(sarr,target,low,mid-1,high);
-		if(sarr[mid].compareTo(target)==-1)
+		if(sarr[mid].compareTo(target)<0)
 			{
-			low=mid+1;
-			mid1=(high+low)/2;
+			low=mid1+1;
 			return search(sarr,target,low,mid1,high); 
 			}
-		if(sarr[mid].compareTo(target)==1)
+		if(sarr[mid].compareTo(target)>0)
 		{
-		high=mid-1;
-		mid1=(high+low)/2;
+		high=mid1-1;
 		return search(sarr,target,low,mid1,high); 
 		}
 			
@@ -31,8 +31,8 @@ public class eleven_five {
 	
 	public static void main(String[] args) {
 		
-		String[] str={"at","","","","ball","","","cat","","","dad","",""};
-		System.out.println(search(str,"ball",0,5,11));
+		String[] str={"at","","","","ball","","","cat","","","dad","","yo"};
+		System.out.println(search(str,"at",0,5,11));
 		
 	}
 
