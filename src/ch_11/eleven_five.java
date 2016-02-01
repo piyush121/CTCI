edf003576d2493521410458774d6e5cb7548c99b
piyush121 <piyushchaudhary07@gmail.com>
2016-02-01 00:14:28 -0500

    Practice

--- src/ch_11/eleven_five.java
+++ src/ch_11/eleven_five.java
@@ -2,37 +2,56 @@ package ch_11;
 
 public class eleven_five {
 	
-	public static int search(String[] sarr,String target,int low, int mid,int high)
+	public static int search(String[] sarr,String target,int low, int high)
 	{
-		int mid1=(high+low)/2;
+		int mid=(high+low)/2;
 		if(low>high)
 			return -1;
+		if(sarr[mid].isEmpty())
+		{
+			int left=mid-1;
+			int right=mid+1;
+			while(left>=low && right <=high)
+			{
+				if(!sarr[left].isEmpty())
+				{
+					mid=left;
+					break;
+				}
+				if(!sarr[right].isEmpty())
+				{
+					mid=right;
+					break;
+				}
+				left--;
+				right++;
+			}
+			
+		}
+		
+		
 		if(sarr[mid].equals(target))
 			return mid;
-		if(sarr[mid]=="" && mid<sarr.length-1)
-			return search(sarr,target,low,mid+1,high);
-		if(sarr[mid]=="" && mid>0)
-			return search(sarr,target,low,mid-1,high);
+		
 		if(sarr[mid].compareTo(target)<0)
 			{
-			low=mid1+1;
-			return search(sarr,target,low,mid1,high); 
+			low=mid+1;
+			return search(sarr,target,low,high); 
 			}
 		if(sarr[mid].compareTo(target)>0)
 		{
-		high=mid1-1;
-		return search(sarr,target,low,mid1,high); 
+		high=mid-1;
+		return search(sarr,target,low,high); 
 		}
 			
 		return -1;
 	}
 	
 	
-	
 	public static void main(String[] args) {
 		
 		String[] str={"at","","","","ball","","","cat","","","dad","","yo"};
-		System.out.println(search(str,"at",0,5,11));
+		System.out.println(search(str,"yo",0,12));
 		
 	}
 
