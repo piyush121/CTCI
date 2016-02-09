f93c66d277f1049565eb7769d439b057109ca0b4
piyush121 <piyushchaudhary07@gmail.com>
2016-02-08 21:04:37 -0500

    Get Rich or Die Tryin'

---
+++ src/ch_11/seventeen_eight.java
@@ -0,0 +1,19 @@
+package ch_11;
+
+public class seventeen_eight {
+
+	public int maxSum(int[] A)
+	{
+		int maxsum=0;
+		int sum=0;
+		for(int i=0;i<A.length;i++)
+		{
+			sum+=A[i];
+			if(maxsum<sum)
+				maxsum=sum;
+			else if(sum<0)
+				sum=0;
+		}
+		return maxsum;
+	}
+}
