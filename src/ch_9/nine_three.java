c465538da0593fe34cc3ec28eae29a9e20ffdbee
piyush121 <piyushchaudhary07@gmail.com>
2016-02-26 10:48:01 -0500

    Get rich or die coding !

---
+++ src/ch_9/nine_three.java
@@ -0,0 +1,38 @@
+package ch_9;
+
+public class nine_three {
+
+	public static int magicIndex(int[] A, int start, int end) {
+		if (start < 0 || end >= A.length || start > end)
+			return -1;
+		int mid = (start + end) / 2;
+		if (A[mid] == mid)
+			return mid;
+		else if (A[mid] > mid) { // magic index cannot be between mid and
+									// A[mid].
+			int left1 = magicIndex(A, A[mid], end);
+			if (left1 != -1)
+				return left1;
+			int right1 = magicIndex(A, start, mid - 1);
+			if (right1 != -1)
+				return right1;
+		} else {//// magic index cannot be between A[mid] and mid.
+			int left2 = magicIndex(A, start, A[mid]);
+			if (left2 != -1)
+				return left2;
+			int right2 = magicIndex(A, mid + 1, end);
+			if (right2 != -1)
+				return right2;
+		}
+
+		return -1;
+
+	}
+
+	public static void main(String[] args) {
+		int[] A = { -10, -1, 0, 2, 2, 3, 4, 7, 9, 12, 13 };
+		System.out.println(magicIndex(A, 0, A.length - 1)); //Should be 7.
+
+	}
+
+}
--- src/ch_9/nine_two.java
+++ src/ch_9/nine_two.java
@@ -1,5 +1,11 @@
 package ch_9;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+import javafx.util.Pair;
+
 /**
  * @author Piyush Chaudhary
  *
@@ -15,6 +21,28 @@ public class nine_two {
 			
 		else
 			return robot(x,y-1)+robot(x-1,y);
+	}
+	
+	public static boolean robotDP(int x,int y,ArrayList<Pair<Integer, Integer>> path,HashMap<Pair<Integer,Integer>,Boolean> cache){
+		
+		if(x<0||y<0)	//checking out of bounds.
+			return false;
+		boolean atOrigin=false;
+		if(x==0 && y==0)
+			atOrigin=true;
+		Pair<Integer,Integer> p=new Pair<>(x,y);
+		if(cache.containsKey(new Pair<>(x,y))) //if we have already checked that a path from this location exist then don't go over it again ! Use the results again.
+			return true;
+		Boolean found=false;
+		if(atOrigin||robotDP(x-1,y,path,cache)||robotDP(x,y-1,path,cache))
+		{	
+			path.add(p);
+			found=true;
+		}
+		cache.put(p,found);
+
+		return found;
+		
 	}
 	public static void main(String[] args) {
 		System.out.println(robot(3,3));
