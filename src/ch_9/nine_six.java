65aea07c962b3e73d9222bc6b04ee9a6c750c28a
piyush121 <piyushchaudhary07@gmail.com>
2016-02-27 13:45:52 -0500

    Coding my way through chapter 9

--- src/ch_9/nine_six.java
+++ src/ch_9/nine_six.java
@@ -1,5 +1,8 @@
 package ch_9;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class nine_six {
 	
 	public static void permuteParenthesis(int open,int close,String str)// easy recursive solution.
@@ -9,13 +12,48 @@ public class nine_six {
 		if(open>0)
 			permuteParenthesis(open-1, close+1, str+"(");//decrease open braces and increase close braces.
 		if(close>0)
-			permuteParenthesis(open, close-1, str+")");
+			permuteParenthesis(open, close-1, str+")"); // str+ operation is a O(n) operation. Be careful. Use string builder instead.
 
+	}
+	
+	public static Set<String> permuteParenthesis1(int num)
+	{	
+		Set<String> result=new HashSet<String>();
+		if(num==0)//base case
+			{
+				result.add("");
+				return result;
+			}
+		else
+			{
+				Set<String> temp=permuteParenthesis1(num-1); // storing result from previous result.
+				for(String str : temp)
+				{
+					for(int i=0;i<str.length();i++)
+					{
+						if(str.charAt(i)=='(')
+						{
+							result.add(insertParen(str, i)); // addinng `()` after every left parenthesis.
+						}
+					}
+					result.add("()"+str); // adding "()" to the beginning.
+				}
+				
+			}
+		return result;
+		
+		
+	}
+	public static String insertParen(String str,int leftindex)
+	{
+		String left=str.substring(0, leftindex+1);
+		String right=str.substring(leftindex+1);
+		return left+"()"+right;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		permuteParenthesis(3, 0, "");
+		System.out.println(permuteParenthesis1(3));
 
 	}
 
