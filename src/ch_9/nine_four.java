39d464a4d6ee1527026b5ce3c646c3fa6289a5d2
piyush121 <piyushchaudhary07@gmail.com>
2016-02-28 13:52:48 -0500

    Coding and more Coding

--- src/ch_9/nine_four.java
+++ src/ch_9/nine_four.java
@@ -1,27 +1,44 @@
 package ch_9;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
-public class nine_four {
-	public static ArrayList<Integer> getsets(ArrayList<Integer> arr,int index)
-	{
-		if(index==0){
-			ArrayList<Integer> temp=new ArrayList<>();
-			temp.add(arr.get(index));
-			return arr;
-		}
-		
-		for(int i=index;i>0;i--){
-			arr.addAll(getsets(arr,index-1));
-		}
+public class nine_four { // Tedious question !
+	public static ArrayList<LinkedList<Integer>> getsets(ArrayList<Integer> arr, int index) {
+		ArrayList<LinkedList<Integer>> result = new ArrayList<>();
 
-		return arr;	
+		if (index == 0) {
+			LinkedList<Integer> list = new LinkedList<>();// adding one element
+															// as base case
+			list.add(arr.get(0));
+			result.add(list);
+			return result;
+		}
+		result = getsets(arr, index - 1);
+		int val = arr.get(index);
+		ArrayList<LinkedList<Integer>> more = new ArrayList<>();
+		for (LinkedList<Integer> list : result) {
+			LinkedList<Integer> list1 = new LinkedList<>();
+			list1.addAll(list);
+			list1.add(val);// append value to every list in the result set.
+			more.add(list1);
+		}
+		result.addAll(more);
+		LinkedList<Integer> temp = new LinkedList<Integer>();
+		temp.add(val);// adding the value itself to the list.
+		result.add(temp);
+
+		return result;
+
 	}
+
 	public static void main(String[] args) {
-		
-		ArrayList<Integer> AL=new ArrayList<>();
-		AL.add(1);AL.add(2);AL.add(3);
-		System.out.println(getsets(AL,AL.size()-1));
+
+		ArrayList<Integer> AL = new ArrayList<>();
+		AL.add(1);
+		AL.add(2);
+		AL.add(3);
+		System.out.println(getsets(AL, AL.size() - 1));
 	}
 
 }
